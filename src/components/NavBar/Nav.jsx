37e449c5dfd7d665b43fd639b80bf1808ebf53ce
Phil Seidel <psbyron3@gmail.com>
2016-07-22 14:05:44 -0700

    [Add] Added features for auth. Includes signin, signup, adn logout html and js

--- src/components/NavBar/Nav.jsx
+++ src/components/NavBar/Nav.jsx
@@ -12,6 +12,25 @@ import { Link } from 'react-router'
 
 import { connect } from 'react-redux';
 import * as actions from '../../actions/actions';
+import RaisedButton from 'material-ui/RaisedButton';
+import RadioButton from 'material-ui/RadioButton';
+import Popover from 'material-ui/Popover/Popover';
+import Menu from 'material-ui/Menu';
+import $ from 'jquery';
+
+const styles = {
+  h3: {
+    marginTop: 20,
+    fontWeight: 400,
+  },
+  block: {
+    display: 'flex',
+  },
+  block2: {
+    margin: 10,
+  },
+};
+
 
 class Nav extends React.Component {
 	constructor(props) {
@@ -20,12 +39,23 @@ class Nav extends React.Component {
 		this.state = {
 			date: moment().format('MM-DD-YY'),
 			open: false,
-			currentSection: 'U.S.'
-		}
+			currentSection: 'U.S.',
+
+      openSignUp: false,
+      anchorOrigin: {
+        horizontal: 'right',
+        vertical: 'bottom',
+      },
+      targetOrigin: {
+        horizontal: 'right',
+        vertical: 'top',
+      },
+    };
 	}
 
 	componentDidMount() {
 		this.props.fetchSections();
+    $('#logoutButton').hide();
 	}
 
 	handleFilter(sectionFilter) {
@@ -34,6 +64,7 @@ class Nav extends React.Component {
 	}
 
 	renderMenuItems() {
+    console.log(this.props.authentication, 'the state inside of Nav')
 		return _.map(this.props.sections, function(num, section) {
 			 return <MenuItem
 			 						checked={section === this.state.currentSection ? true : false}
@@ -67,6 +98,155 @@ class Nav extends React.Component {
 		this.setState({open: false});
 	}
 
+ 
+  handleTouchTap = (event) => {
+    // This prevents ghost click.
+    event.preventDefault();
+    this.setState({
+      openSignUp: true,
+      anchorEl: event.currentTarget
+    });
+    
+  };
+
+  handleTouchSignin = (event) => {
+    event.preventDefault();
+    this.setState({
+      openSignIn : true,
+      anchorEl : event.currentTarget
+    });
+  }
+
+  handleRequestClose = () => {
+    this.setState({
+      openSignUp: false,
+      openSignIn: false,
+      name    : '',
+      username: '',
+      password: ''
+    });
+  };
+
+  setAnchor = (positionElement, position) => {
+    const {anchorOrigin} = this.state;
+    anchorOrigin[positionElement] = position;
+
+    this.setState({
+      anchorOrigin: anchorOrigin,
+    });
+  };
+
+  setTarget = (positionElement, position) => {
+    const {targetOrigin} = this.state;
+    targetOrigin[positionElement] = position;
+
+    this.setState({
+      targetOrigin: targetOrigin,
+    });
+  };
+
+  onNameInputChange(event) {
+    console.log("username input change!!")
+    this.setState({
+      name: event.target.value
+    })
+  };
+
+
+  onUsernameInputChange(event) {
+  	console.log("username input change!!")
+  	this.setState({
+  		username: event.target.value
+  	})
+  };
+
+  onPasswordInputChange(event) {
+  	this.setState({
+  		password: event.target.value
+  	})
+  };
+
+  onSignupSubmit(event) {
+  	event.preventDefault();
+    this.props.submitUsernameAndPassword({
+      name    : this.state.name,
+      username: this.state.username,
+      password: this.state.password
+    });
+    this.handleRequestClose();
+    // this.loginButton();
+    //this.showLogOut();
+  };
+
+  onSigninSubmit(event) {
+    event.preventDefault();
+    this.props.submitSignin({
+      username: this.state.username,
+      password: this.state.password
+    });
+    console.log("props in logout : ", this.props)
+    this.handleRequestClose();
+    //this.showLogOut();
+  };
+
+  // showLogOut() {
+  //   $('#signup').hide()
+  //   $('#signin').hide()
+  //   $('#logoutButton').show()
+  // }
+
+  // hideLogOut() {
+  //   $('#signup').show()
+  //   $('#signin').show()
+  //   $('#logoutButton').hide()
+  // }
+
+  onLogout() {
+    console.log("props in logout : ", this.props)
+    console.log("localStorage : ", window.localStorage.token)
+    window.localStorage.token = ''
+    this.props.logoutUser();
+    // if(this.props.authentication.authentication === false) {
+    //   this.hideLogOut();
+    // }
+  }
+
+  loginButton() {
+    console.log("in loginButton")
+    if(this.props.authentication.authentication === false){
+      console.log('true');
+      return (
+        <div>
+            <span id="signin">
+              <RaisedButton
+                id="signinButton"
+                onTouchTap={this.handleTouchSignin}
+                label="Sign In"
+              />
+            </span>
+
+            <span id="signup">
+              <RaisedButton
+                id="signupButton"
+                onTouchTap={this.handleTouchTap}
+                label="Sign Up"
+              />
+            </span>
+          </div>
+        )
+    } else {
+      return (
+          <div id="logoutButton">
+              <div>Welcome, {this.props.authName}!</div>
+              <RaisedButton
+                onTouchTap={this.onLogout.bind(this)}
+                label="Log Out"
+              />
+          </div>
+          )
+    }
+  }
+
 	render() {
 
 
@@ -84,25 +264,118 @@ class Nav extends React.Component {
 					    title = {AboutLink}
 					    onLeftIconButtonTouchTap = { this.handleToggle.bind(this) }
 					    style = {
-					    	{ 'backgroundColor': '#fff' }
-					    	}
-					    iconElementRight = {
-					    	<DatePicker
-									onChange = { this.handleDateChange.bind(this) }
-									minDate = {new Date(2016, 6, 12)}
-									maxDate = { new Date() }
-									dateTimeFormat = { this.handleDate.bind(this) }
-					    		formatDate = { this.handleDate.bind(this) }
-					    		defaultDate = { new Date() }
-									inputStyle = {{
-										'color': '#141414',
-										'textAlign': 'right',
-									}}
-						    	autoOk = { true }
-						    />
-						}
+					    		{ 'backgroundColor': '#fff' }
+					    }
 					>
-						<h id="centerIcon">{ this.state.currentSection }</h>
+					<div id="rightButtons">
+            {this.loginButton.bind(this)()}
+            </div>
+
+
+						<div id="centerIcon">
+							<h id="centerSection">{ this.state.currentSection } </h>
+							<span id="centerDate">
+								<DatePicker
+										onChange = { this.handleDateChange.bind(this) }
+										minDate = {new Date(2016, 6, 12)}
+										maxDate = { new Date() }
+										dateTimeFormat = { this.handleDate.bind(this) }
+						    		formatDate = { this.handleDate.bind(this) }
+						    		defaultDate = { new Date() }
+										inputStyle = {{
+											'color': '#141414'
+										}}
+							    	autoOk = { true }
+							    />
+							 </span>
+						</div>
+						 <Popover
+			          open={this.state.openSignUp}
+			          anchorEl={this.state.anchorEl}
+			          anchorOrigin={this.state.anchorOrigin}
+			          targetOrigin={this.state.targetOrigin}
+			          onRequestClose={this.handleRequestClose}
+			        >
+              <div>
+
+                <div id="signupModal">
+                  <div id='signupName'>
+                    <form onSubmit={this.onSignupSubmit.bind(this)}>    
+                      <input value={this.state.name} onChange={this.onNameInputChange.bind(this)} type='text' placeholder='Name'></input>       
+                    </form>
+                  </div>   
+                  <div id='signupUser'>
+    			        	<form onSubmit={this.onSignupSubmit.bind(this)}>   	
+    				         	<input value={this.state.username} onChange={this.onUsernameInputChange.bind(this)} type='text' placeholder='Username'></input>      	
+    				 				</form>
+                  </div>
+                  <div id='signupPass'>
+    				 				<form onSubmit={this.onSignupSubmit.bind(this)}>	         
+    					        <input value={this.state.password} onChange={this.onPasswordInputChange.bind(this)} type='password' placeholder='Password'></input>
+    				 				</form>
+                  </div>
+                </div>
+                <div id='signupButtons'>
+                    <span id="signupConfirm">
+                      <button 
+                        className='btn btn-default'
+                        onClick={this.onSignupSubmit.bind(this)}
+                      >Sign Up
+                      </button>
+                    </span>
+
+                    <span id='signupCancel'>
+                      <button 
+                        className='btn btn-default'
+                        onClick={this.handleRequestClose}
+                      >Cancel
+                      </button>
+                    </span>
+                </div>  
+              </div>
+								
+			        </Popover>
+
+              <Popover
+                open={this.state.openSignIn}
+                anchorEl={this.state.anchorEl}
+                anchorOrigin={this.state.anchorOrigin}
+                targetOrigin={this.state.targetOrigin}
+                onRequestClose={this.handleRequestClose}
+              >
+              <div>
+                <div id="signinModal">
+                  <div id="signinUser">
+                    <form onSubmit={this.onSigninSubmit.bind(this)}>    
+                      <input value={this.state.username} onChange={this.onUsernameInputChange.bind(this)} type='text' placeholder='Username'></input>       
+                    </form>
+                  </div>
+                  <div id="signinPass">
+                    <form onSubmit={this.onSigninSubmit.bind(this)}>           
+                          <input value={this.state.password} onChange={this.onPasswordInputChange.bind(this)} type='password' placeholder='Password'></input>
+                  
+                    </form>
+                  </div>
+                </div>
+                <div id='signinButtons'>
+                  <span id="signinConfirm">
+                    <button 
+                      className='btn btn-default'
+                      onClick={this.onSigninSubmit.bind(this)}
+                    >Sign In
+                    </button>
+                  </span>
+                  <span id='signinCancel'>
+                    <button 
+                      className='btn btn-default'
+                      onClick={this.handleRequestClose}
+                    >Cancel
+                    </button>
+                  </span>
+                </div>
+              </div>
+                
+              </Popover>
 					</AppBar>
 				</MuiThemeProvider>
 				<MuiThemeProvider>
@@ -123,8 +396,14 @@ class Nav extends React.Component {
 function mapStateToProps(state) {
 	return {
 		sections: state.fetchSectionsReducer.data,
-		articles: state.fetchArticlesReducer.data
+		articles: state.fetchArticlesReducer.data,
+    authentication: state.authenticationReducer,
+    authName: state.authNameReducer
 	};
 }
 
 export default connect(mapStateToProps, actions)(Nav);
+
+//****************************************************************************
+
+

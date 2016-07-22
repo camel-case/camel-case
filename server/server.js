0e30c81b89aecf1adfadc84a67efc4f26547567c
Phil Seidel <psbyron3@gmail.com>
2016-07-22 14:03:51 -0700

    [Add] served auth routes

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
-var express = require('express')
+var express = require('express');
+var auth = require('./routes/authRoute.js');
+var bodyParser = require('body-parser');
+
 
 var app = express();
 
+app.use(bodyParser.json())
 /* Serve all static files from public folder */
 app.use(express.static('./client'));
+
+// route for signup to db
+app.use('/auth', auth);
 
 /* Include routes */
 require('./routes/routes')(app);
